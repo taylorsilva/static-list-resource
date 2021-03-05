70775a34251aff630bee6dfefd0288a7e676ed81
Taylor Silva <taylorsilva@outlook.com>
2021-03-05 13:10:11 -0500

    Convert check tests to table tests
    Signed-off-by: Taylor Silva <taylorsilva@outlook.com>

--- check_test.go
+++ check_test.go
@@ -14,79 +14,75 @@ type CheckTestSuite struct {
 	*require.Assertions
 }
 
-func (c *CheckTestSuite) TestInitialCheck() {
-	request := resource.CheckRequest{
-		Source: resource.Source{
-			List: []string{"item1", "item2", "item3", "item4", "item5"},
+func (c *CheckTestSuite) TestCheck() {
+	tests := []struct {
+		Description     string
+		Request         resource.CheckRequest
+		ExpectedVersion resource.Version
+		ExpctedErr      string
+	}{
+		{
+			Description: "given no version, it should return the first item",
+			Request: resource.CheckRequest{
+				Source: resource.Source{
+					List: []string{"item1", "item2", "item3", "item4", "item5"},
+				},
+			},
+			ExpectedVersion: resource.Version{Item: "item1"},
+		},
+		{
+			Description: "should return the next item: given item3 it should return item4",
+			Request: resource.CheckRequest{
+				Source: resource.Source{
+					List: []string{"item1", "item2", "item3", "item4", "item5"},
+				},
+				Version: resource.Version{Item: "item3"},
+			},
+			ExpectedVersion: resource.Version{Item: "item4"},
+		},
+		{
+			Description: "given the last item in the list, it should return the first item",
+			Request: resource.CheckRequest{
+				Source: resource.Source{
+					List: []string{"item1", "item2", "item3", "item4", "item5"},
+				},
+				Version: resource.Version{Item: "item5"},
+			},
+			ExpectedVersion: resource.Version{Item: "item1"},
+		},
+		{
+			Description: "first item in list should be returned if given version is not found",
+			Request: resource.CheckRequest{
+				Source: resource.Source{
+					List: []string{"item1", "item2", "item3", "item4", "item5"},
+				},
+				Version: resource.Version{Item: "item6"},
+			},
+			ExpectedVersion: resource.Version{Item: "item1"},
+		},
+		{
+			Description: "first item in list should be returned if given version is not found",
+			Request: resource.CheckRequest{
+				Source: resource.Source{
+					List: []string{},
+				},
+				Version: resource.Version{Item: "item2"},
+			},
+			ExpctedErr: "empty list provided in resouce's Source. At least one item required",
 		},
 	}
-	check := resource.NewCheck()
-	response, _ := check.Run(request)
-	expected := resource.CheckResponse{
-		resource.Version{Item: "item1"},
-	}
-	c.Equal(expected[0].Item, response[0].Item, "given no version, it should return the first item")
-	c.NotEqual(time.Time{}, response[0].Date, "time is not nil/default time.Time")
-}
 
-func (c *CheckTestSuite) TestReturnNextItem() {
-	request := resource.CheckRequest{
-		Source: resource.Source{
-			List: []string{"item1", "item2", "item3", "item4", "item5"},
-		},
-		Version: resource.Version{Item: "item3"},
+	for _, test := range tests {
+		check := resource.NewCheck()
+		response, err := check.Run(test.Request)
+		if response != nil {
+			c.Equal(test.ExpectedVersion.Item, response[0].Item, test.Description)
+			c.NotEqual(time.Time{}, response[0].Date, "time is not nil/default time.Time")
+		}
+		if test.ExpctedErr != "" {
+			c.EqualError(err, test.ExpctedErr)
+		}
 	}
-	check := resource.NewCheck()
-	response, _ := check.Run(request)
-	expected := resource.CheckResponse{
-		resource.Version{Item: "item4"},
-	}
-	c.Equal(expected[0].Item, response[0].Item, "given item3 it should return item4")
-	c.NotEqual(time.Time{}, response[0].Date, "time is not nil/default time.Time")
-}
-
-func (c *CheckTestSuite) TestReturnFirstItemWhenEndIsReached() {
-	request := resource.CheckRequest{
-		Source: resource.Source{
-			List: []string{"item1", "item2", "item3", "item4", "item5"},
-		},
-		Version: resource.Version{Item: "item5"},
-	}
-	check := resource.NewCheck()
-	response, _ := check.Run(request)
-	expected := resource.CheckResponse{
-		resource.Version{Item: "item1"},
-	}
-	c.Equal(expected[0].Item, response[0].Item, "given the last item in the list, it should return the first item")
-	c.NotEqual(time.Time{}, response[0].Date, "time is not nil/default time.Time")
-}
-
-func (c *CheckTestSuite) TestLastVersionRemovedFromList() {
-	request := resource.CheckRequest{
-		Source: resource.Source{
-			List: []string{"item1", "item2", "item3", "item4", "item5"},
-		},
-		Version: resource.Version{Item: "item6"},
-	}
-	check := resource.NewCheck()
-	response, _ := check.Run(request)
-	expected := resource.CheckResponse{
-		resource.Version{Item: "item1"},
-	}
-	c.Equal(expected[0].Item, response[0].Item, "first item in list should be returned if given version not found")
-	c.NotEqual(time.Time{}, response[0].Date, "time is not nil/default time.Time")
-}
-
-func (c *CheckTestSuite) TestErrorIfListIsEmpty() {
-	request := resource.CheckRequest{
-		Source: resource.Source{
-			List: []string{},
-		},
-		Version: resource.Version{Item: "item2"},
-	}
-	check := resource.NewCheck()
-	_, err := check.Run(request)
-	c.EqualError(err, "empty list provided in resouce's Source. At least one item required")
 }
 
 func TestCheckSuite(t *testing.T) {
